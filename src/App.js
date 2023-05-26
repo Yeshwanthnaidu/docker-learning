97dd4a0cf83c84f388dcac1519ca36c07dd84bba
yeshwanth_fisson <103189029+Yeshwanthnaidu@users.noreply.github.com>
2023-05-26 20:13:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const clearAll = () => {
+    setName('');
+    setEmail('');
+    setPhoneNumber('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Form style={{ margin: '20px', width: '50vw' }}>
+        <Form.Group className="mb-3">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Jhon" value={name} onChange={(e) => { setName(e.target.value) }} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control type="number" value={phoneNumber} onChange={(e) => { setPhoneNumber(e.target.value) }} />
+        </Form.Group>
+      </Form>
+      <button onClick={() => { alert(`name: ${name}, email: ${email}, phoneNumber: ${phoneNumber}`) }} style={{ margin: '20px' }}>Submit</button>
+      <button onClick={clearAll}>Clear All</button>
+    </>
   );
 }
 
 export default App;
+
